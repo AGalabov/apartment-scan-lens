0e51062e002fe2fd606f8eb5f17de831b5434a0d
Alexander Galabov <galabovalexandar@gmail.com>
2025-02-06 12:01:46 +0200

    added support for other websites

--- src/utils/metadataService.ts
+++ src/utils/metadataService.ts
@@ -13,18 +13,40 @@ export async function fetchMetadata(url: string): Promise<MetadataResult> {
   try {
     const response = await fetch(proxyUrl + url);
     const uint8array = new Uint8Array(await response.arrayBuffer());
-    const decoder = new TextDecoder('windows-1251');
-    const body = decoder.decode(uint8array);
+
+    // UTF8 body
+    const utf8Decoder = new TextDecoder();
+    const utf8Body = utf8Decoder.decode(uint8array);
     const domParser = new DOMParser();
-    const doc = domParser.parseFromString(body, 'text/html');
+    let doc = domParser.parseFromString(utf8Body, 'text/html');
+
+    const contentType = doc
+      .querySelector('head > meta[http-equiv="Content-Type"]')
+      ?.getAttribute('content');
+
+    const match = contentType?.match(/charset=([^;]*)/);
+    const encoding = match?.[1];
+
+    if (encoding && encoding !== 'utf-8') {
+      const decoder = new TextDecoder(encoding);
+      const body = decoder.decode(uint8array);
+
+      doc = domParser.parseFromString(body, 'text/html');
+    }
 
     const title = doc.querySelector('head > title')?.textContent;
     const image = doc
       .querySelector('head > meta[property="og:image"]')
       ?.getAttribute('content');
 
-    metaData.title = title ? title : undefined;
-    metaData.imageUrl = image ? 'https:' + image : undefined;
+    console.log(image, image?.startsWith('https:'));
+
+    metaData.title = title?.trim();
+    metaData.imageUrl = image
+      ? image.startsWith('https:')
+        ? image
+        : 'https:' + image
+      : undefined;
   } catch (error) {
     console.error(`Error fetching metadata: ${error}`);
   }
